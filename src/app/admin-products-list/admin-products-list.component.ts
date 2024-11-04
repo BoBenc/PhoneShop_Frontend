9ff184802cd374fa831b059349328f77f36d6c09
BoBenc <bogerbence@ktch.hu>
2024-11-04 13:43:49 +0100

    Kezdes

---
+++ src/app/admin-products-list/admin-products-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-products-list',
+  templateUrl: './admin-products-list.component.html',
+  styleUrl: './admin-products-list.component.css'
+})
+export class AdminProductsListComponent {
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { AdminProductsListComponent } from './admin-products-list/admin-products-list.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsListComponent,
+    AdminProductsListComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
