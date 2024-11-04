1da86eea1d7cbed0402ae812010622a1305e87c1
BoBenc <bogerbence@ktch.hu>
2024-11-04 14:44:45 +0100

    Routing, base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { AdminProductsListComponent } from './admin-products-list/admin-products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component:LoginComponent },
+  { path: "register", component:RegisterComponent },
+  { path: "home", component:HomeComponent },
+  { path: "products", component:ProductsListComponent },
+  { path: "admin", component:AdminProductsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/base-service.service.ts
+++ src/app/base-service.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseServiceService {
+  refPhoneShop: AngularFireList<any>
+  
+  constructor(private db:AngularFireDatabase) {
+    this.refPhoneShop = db.list("/products")
+  }
 
-  constructor() { }
+  addPhone(phone:any) {
+    this.refPhoneShop.push(phone)
+  }
+
+  getPhone() {
+    return this.refPhoneShop
+  }
+
+  deletePhone(phone:any) {
+    this.refPhoneShop.remove(phone.key)
+  }
+  updatePhone(phone:any) {
+    let key = phone.key
+    delete phone.key
+    this.refPhoneShop.update(key, phone)
+  }
 }
